2ebbe7b1d1fe0dce3662e20ffaf18eca9be1e095
MiGaNg <miguelangelyv1@gmail.com>
2026-01-30 12:04:02 -0500

    Rutas de la aplicación

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router'
+import { LoginPage } from '../auth/pages/LoginPage';
+import { CalendarPage } from '../calendar/pages/CalendarPage';
+
+export const AppRouter = () => {
+
+    const authStatus = 'not-authenticated';
+
+    return (
+
+        <Routes>
+            {
+                (authStatus === 'not-authenticated')
+                    ? <Route path='/auth/*' element={<LoginPage />} />
+                    : <Route path='/*' element={<CalendarPage />} />
+            }
+            <Route path='/*' element={<Navigate to='/auth/login' />} />
+        </Routes>
+    )
+}
