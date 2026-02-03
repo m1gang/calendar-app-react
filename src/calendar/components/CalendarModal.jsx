84e4c63774d9894bd8c0e1a42f5d73a5bf86686a
MiGaNg <miguelangelyv1@gmail.com>
2026-02-03 10:53:14 -0500

    Datepicker en español

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -2,8 +2,11 @@ import { addHours } from "date-fns";
 import { useState } from "react";
 
 import Modal from "react-modal"
-import DatePicker from "react-datepicker";
+import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { es } from 'date-fns/locale/es';
+
+registerLocale('es', es)
 
 const customStyles = {
     content: {
@@ -70,6 +73,9 @@ export const CalendarModal = () => {
                         className="form-control"
                         onChange={(event) => onDateChanged(event, 'start')}
                         dateFormat='Pp'
+                        showTimeSelect
+                        locale="es"
+                        timeCaption="Hora"
                     />
                 </div>
 
@@ -82,6 +88,9 @@ export const CalendarModal = () => {
                         className="form-control"
                         onChange={(event) => onDateChanged(event, 'end')}
                         dateFormat='Pp'
+                        showTimeSelect
+                        locale="es"
+                        timeCaption="Hora"
                     />
                 </div>
 
