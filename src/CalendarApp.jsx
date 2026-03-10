0b73fe1907db20ae315ba75414b766ad4f6104c6
MiGaNg <miguelangelyv1@gmail.com>
2026-03-10 18:21:24 -0500

    Instalación y configuración de Redux

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from "react-router-dom"
 import { AppRouter } from "./router/AppRouter"
+import { store } from "./store"
+import { Provider } from "react-redux"
 
 export const CalendarApp = () => {
     return (
+        <Provider store={store}>
         <BrowserRouter>
             <AppRouter />
         </BrowserRouter>
+        </Provider>
     )
 }
