df0f8513ea0ffdd756bf24f73535025f0a020dcb
MiGaNg <miguelangelyv1@gmail.com>
2026-03-10 18:05:19 -0500

    Validaciones del formulario

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,6 +1,7 @@
 import { addHours, differenceInSeconds } from "date-fns";
-import { useState } from "react";
-
+import { useMemo, useState } from "react";
+import Swal from "sweetalert2";
+import 'sweetalert2/dist/sweetalert2.min.css';
 import Modal from "react-modal"
 import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -25,12 +26,23 @@ Modal.setAppElement('#root');
 export const CalendarModal = () => {
 
     const [isOpen, setIsOpen] = useState(true);
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    
     const [formValues, setFormValues] = useState({
         title: 'Miguel',
         notes: 'Yapias',
         start: new Date(),
         end: addHours(new Date(), 2),
-    })
+    });
+
+    const titleClass = useMemo(() => {
+        if(!formSubmitted) return '';
+
+        return formValues.title.length > 0
+        ? ''
+        : 'is-invalid'
+
+    }, [formValues.title,formSubmitted])
 
     const onInputChange = ({ target }) => {
         setFormValues({
@@ -53,9 +65,12 @@ export const CalendarModal = () => {
 
     const onSubmit = (event) => {
         event.preventDefault();
+        setFormSubmitted(true);
+
         const difference = differenceInSeconds(formValues.end , formValues.start);
+        
         if(isNaN(difference) || difference<=0){
-            console.log("Error en fechas");
+            Swal.fire('Fechas incorrecta','Revisar las fechas ingresadas','error');
             return;
             
         }
@@ -118,7 +133,7 @@ export const CalendarModal = () => {
                     <label>Titulo y notas</label>
                     <input
                         type="text"
-                        className="form-control"
+                        className={`form-control ${titleClass}`}
                         placeholder="Título del evento"
                         name="title"
                         autoComplete="off"
