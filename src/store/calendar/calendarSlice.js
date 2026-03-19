53ff0598a612d68025a389aa1765fd6195c27b3e
MiGaNg <miguelangelyv1@gmail.com>
2026-03-19 16:34:18 -0500

    Editar el evento activo

--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -1,36 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { addHours } from 'date-fns';
+import { createSlice } from "@reduxjs/toolkit";
+import { addHours } from "date-fns";
 
 const tempEvent = {
-    _id:new Date().getTime(), 
-    title: 'Cumpleaños',
-    notes: 'Hay que comprar una torta',
-    start: new Date(),
-    end: addHours(new Date(), 2),
-    bgColor: '#eb4b4c',
-    user: {
-        _id: '123',
-        name: 'Miguel'
-    }
-}
+  _id: new Date().getTime(),
+  title: "Cumpleaños",
+  notes: "Hay que comprar una torta",
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: "#eb4b4c",
+  user: {
+    _id: "123",
+    name: "Miguel",
+  },
+};
 
 export const calendarSlice = createSlice({
-    name: 'calendar',
-    initialState: {
-        events: [tempEvent],
-        activeEvent: null,
+  name: "calendar",
+  initialState: {
+    events: [tempEvent],
+    activeEvent: null,
+  },
+  reducers: {
+    onSetActiveEvent: (state, { payload }) => {
+      state.activeEvent = payload;
     },
-    reducers: {
-        onSetActiveEvent: (state,{payload}) => {
-            state.activeEvent = payload;
-        },
-        onAddNewEvent:(state,{payload})=> {
-            state.events.push(payload);
-            state.activeEvent = null;
+    onAddNewEvent: (state, { payload }) => {
+      state.events.push(payload);
+      state.activeEvent = null;
+    },
+    onUpdateEvent: (state, { payload }) => {
+      state.events = state.events.map((event) => {
+        if (event._id === payload._id) {
+          return payload;
         }
-    }
+        return event;
+      });
+    },
+  },
 });
 
-
 // Action creators are generated for each case reducer function
-export const { onSetActiveEvent,onAddNewEvent } = calendarSlice.actions;
+export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent } =
+  calendarSlice.actions;
