68746847335def3aecc28e68a7e1a709474c9788
MiGaNg <miguelangelyv1@gmail.com>
2026-06-15 16:01:01 -0500

    Creando variables de entorno

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router'
-import { LoginPage } from '../auth/pages/LoginPage';
-import { CalendarPage } from '../calendar/pages/CalendarPage';
+import React from "react";
+import { Navigate, Route, Routes } from "react-router";
+import { LoginPage } from "../auth/pages/LoginPage";
+import { CalendarPage } from "../calendar/pages/CalendarPage";
+import { getEnvVariables } from "../helpers/getEnvVariables";
 
 export const AppRouter = () => {
+  const authStatus = "authenticated";
 
-    const authStatus = 'authenticated';
-
-    return (
-
-        <Routes>
-            {
-                (authStatus === 'not-authenticated')
-                    ? <Route path='/auth/*' element={<LoginPage />} />
-                    : <Route path='/*' element={<CalendarPage />} />
-            }
-            <Route path='/*' element={<Navigate to='/auth/login' />} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      {authStatus === "not-authenticated" ? (
+        <Route path="/auth/*" element={<LoginPage />} />
+      ) : (
+        <Route path="/*" element={<CalendarPage />} />
+      )}
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
+    </Routes>
+  );
+};
